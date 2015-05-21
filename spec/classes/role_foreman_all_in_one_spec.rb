543384bcb1951303c3720e5444c12133f4890efa
Trey Dockendorf <treydock@gmail.com>
2015-05-20 19:51:37 -0500

    * Fix tests to work with concat module by spoofing concat_basedir fact

--- spec/classes/role_foreman_all_in_one_spec.rb
+++ spec/classes/role_foreman_all_in_one_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe 'role::foreman_all_in_one' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
-      let(:facts) { facts }
+      let(:facts) do
+        facts.merge({
+          :concat_basedir => '/dne',
+        })
+      end
 
       it { should compile.with_all_deps }
       it { should create_class('role::foreman_all_in_one') }
